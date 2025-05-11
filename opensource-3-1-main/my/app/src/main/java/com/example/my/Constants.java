a7fb2b24f3cae48aefa08b749abfd9e7f7fd674b
dhfqoal <jkh041382@gmail.com>
2025-05-11 14:46:08 +0900

    custom, ex_data, setting upload

---
+++ opensource-3-1-main/my/app/src/main/java/com/example/my/Constants.java
@@ -0,0 +1,10 @@
+package com.example.my;
+
+public class Constants {
+    // API 기본 URL을 새로운 IP 주소로 변경
+    private static final String BASE_URL = "http://172.30.66.131:8080";
+    
+    // API 엔드포인트
+    public static final String API_USERS = BASE_URL + "/api/users";
+    public static final String API_WORKOUTS = BASE_URL + "/api/workouts";
+} 
